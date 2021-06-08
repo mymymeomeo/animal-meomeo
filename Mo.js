4ea08c52af5c3086afed8f841ae42004e8672d62
dung.vl <deepeducationvn@gmail.com>
2021-06-08 20:51:50 +0700

    new commit on new branch

---
+++ Mo.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class s extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> Mo </Text>
+      </View>
+    );
+  }
+}
+
+export default s;
